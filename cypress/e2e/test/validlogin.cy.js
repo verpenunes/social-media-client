49f37d69d1fb3a5c48a411b5abc96ae0d91f6321
verpenunes <106631829+verpenunes@users.noreply.github.com>
2024-10-16 21:29:23 +0200

    configured cypress

---
+++ cypress/e2e/test/validlogin.cy.js
@@ -0,0 +1,41 @@
+/* eslint-disable no-undef */
+import { validEmail, validPassword } from '../../support/testdata';
+
+describe('Login with valid credentials', () => {
+  it('User can log in with valid credentials', () => {
+    // Visit the login page
+    cy.visit('https://bekzatbagdat.github.io/social-media-client/');
+
+    // Ensuring the register modal is not visible
+    cy.get('#registerModal').then($modal => {
+      if ($modal.is(':visible')) {
+        //close the modal
+        cy.get('#registerModal').click('topRight');
+      }
+    });
+
+    // the login button is visible before clicking
+    cy.get('button[data-auth="login"][data-bs-toggle="modal"]').should(
+      'be.visible'
+    );
+
+    // Click the login button to open the login modal
+    cy.get('button[data-auth="login"][data-bs-toggle="modal"]')
+      .eq(1)
+      .click();
+
+    // Fill in valid credentials
+    cy.get('#loginEmail').type(validEmail);
+    cy.get('#loginPassword').type(validPassword);
+
+    // Click the login button in the form
+    cy.get('#loginForm button')
+      .contains('Login')
+      .click();
+
+    // Verifying that the Logout button is visible after login
+    cy.get('button')
+      .contains('Logout')
+      .should('be.visible');
+  });
+});
---
+++ cypress/support/testdata.js
@@ -0,0 +1,9 @@
+export { validEmail, validPassword, invalidEmail, invalidPassword };
+
+// User credentials for testing
+
+const validEmail = 'validemail0101@noroff.no';
+const validPassword = 'validPassword';
+
+const invalidEmail = 'invalid@noroff.no';
+const invalidPassword = 'invalidPassword';
